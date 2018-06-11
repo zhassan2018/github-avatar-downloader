ddd91955fd04eea0989d0ea8061fab5aa753e395
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-11 21:53:12 +0000

    Adding all your changes

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
 var request = require('request');
+var token =  'd3b776e534adf8c70362e68792904f4bf8afd84c'
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : token
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
