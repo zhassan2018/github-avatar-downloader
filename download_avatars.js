e0d61ace82895bde1177638d4778f5ff5fc3861f
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-06-12 00:40:08 +0000

    images downloaded

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
-var token =  require("./secrets")
+var token =  require("./secrets");
+var fs = require('fs');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -7,7 +8,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization' : token.GITHUB_TOKEN
+      'Authorization' : token.GITHUB_TOKEN 
     }
   };
 
@@ -22,7 +23,14 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   for (var i = 0; i < result.length; i++){
   	console.log(result[i].avatar_url);
+  	downloadImageByURL(result[i].avatar_url, "avatar_"+i+".jpg")
   }
 });
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
+  .pipe(fs.createWriteStream(filePath))
+}
 
 
